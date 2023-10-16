770e6604292c9d343355463006fce41d0fb4249a
bojangjorgjievski4 <147811608+bojangjorgjievski4@users.noreply.github.com>
2023-10-16 18:17:11 +0200

    Add files via upload

---
+++ project1/src/smetka.java
@@ -0,0 +1,14 @@
+
+public class smetka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int jab=32;
+int komp=20;
+double pras=50;
+double x=(jab*2)+(komp*5)+(pras*2.5);	
+System.out.println("SMETKA");
+System.out.println(x);
+}
+
+}
---
+++ project2/src/smetka.java
@@ -0,0 +1,14 @@
+
+public class menuvacnica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=100;
+		double evro=61.7;
+		double rezultat=x/evro;
+		System.out.println (rezultat);
+		System.out.println ("smetka");
+		
+	}
+
+}
