9d3f57a3d821918af5eb34465802569cc5d33a66
bojangjorgjievski4 <147811608+bojangjorgjievski4@users.noreply.github.com>
2023-10-27 11:00:48 +0200

    Add files via upload

---
+++ celsiustofahrenheit/src/celsiustofahrenheit/celsiustofahrenheit.java
@@ -0,0 +1,18 @@
+package celsiustofahrenheit;
+
+import java.util.Scanner;
+
+public class main {
+	
+    public static void main (String[] args)
+{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Vnesete temperatura vo celzius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = (9.0/5.0) * celsius + 32.0;
+
+        System.out.println("Temperaturata vo farenhajt e : " + fahrenheit);
+    }
+}
